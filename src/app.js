a92a0cebbf31d8b56709b7965f6d1fc773d526ad
Erika Suárez Valencia <1734998+erikasv@users.noreply.github.com>
2021-10-29 16:46:23 +0000

    Add koop and koop-provider-pg

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const Koop = require('koop')
+const provider = require('koop-provider-pg')
+
 const app = express()
 const port = 3000
+
+const koop = new Koop()
+koop.register(provider)
+
+app.use('/koop', koop.server)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
